2763ea9a3ba7914265c98473742ce28ff0e66cc1
Dong <Ddong@163.com>
2022-10-15 19:57:33 +0800

    third commit

--- src/main/java/com/example/gittest/test.java
+++ src/main/java/com/example/gittest/test.java
@@ -10,5 +10,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello!Git");
         System.out.println("result");
+        System.out.println("spring");
     }
 }
