c76b63015039811fffbf195266a1ec9f0ae5ac1d
Dong <Ddong@163.com>
2022-10-15 19:46:50 +0800

    first commit

---
+++ src/main/java/com/example/gittest/test.java
@@ -0,0 +1,13 @@
+package com.example.gittest;
+
+/**
+ * @author 赵
+ * @program: git-test
+ * @description:
+ * @date 2022-10-15 19:39:00
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello!Git");
+    }
+}
